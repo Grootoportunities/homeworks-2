20b5f63de8c7f0140968f6745aa020289f4fa484
Grootoportunities <tapir1337@mail.ru>
2024-04-06 16:34:49 +0300

    HW8 Done

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,20 +1,43 @@
-import {UserType} from '../HW8'
+import { UserType } from "../HW8";
 
-type ActionType =
-    | { type: 'sort'; payload: 'up' | 'down' }
-    | { type: 'check'; payload: number }
+type SortActionType = { type: "sort"; payload: "up" | "down" };
+type CheckActionType = { type: "check"; payload: number };
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
-    switch (action.type) {
-        case 'sort': { // by name
+type ActionType = SortActionType | CheckActionType;
 
-            return state // need to fix
-        }
-        case 'check': {
+export const homeWorkReducer = (
+  state: UserType[],
+  action: ActionType,
+): UserType[] => {
+  // need to fix any
+  switch (action.type) {
+    case "sort": {
+      // by name
 
-            return state // need to fix
-        }
-        default:
-            return state
+      return [...state].sort((a, b) =>
+        action.payload === "up"
+          ? a.name > b.name
+            ? 1
+            : -1
+          : a.name < b.name
+            ? 1
+            : -1,
+      );
     }
-}
+    case "check": {
+      return [...state].filter((u) => u.age >= action.payload);
+    }
+    default:
+      return state;
+  }
+};
+
+export const sortAC = (payload: "up" | "down"): SortActionType => ({
+  type: "sort",
+  payload,
+});
+
+export const checkAC = (payload: number): CheckActionType => ({
+  type: "check",
+  payload,
+});
